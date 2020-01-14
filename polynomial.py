ed1582ed25886a221c4b5232ae433b5a75b0d600
sai-advaith <advaith.maddipatla@gmail.com>
2020-01-14 17:58:51 +0000

    minor changes to be done

--- polynomial.py
+++ polynomial.py
@@ -1,1 +1,11 @@
-print("hi")
+class polynomial(object):
+    def __init__(self,list):
+        self.list = list # list is a collection of the coefficients of the polynomial P(x)
+    def order(self):
+        return (len(self.list)-1)
+    def add(self,poly2):
+        k = max(order(self.list),order(poly2))
+        net = []
+        for i in range(k):
+            net.append(self.list[i] + poly2[i])
+        return net
