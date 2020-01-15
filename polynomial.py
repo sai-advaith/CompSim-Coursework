ef0679b10c51018b1f0f285320c602ec9142339d
Advaith Sai <advaith.maddipatla@gmail.com>
2020-01-15 11:03:18 +0000

    c1

--- polynomial.py
+++ polynomial.py
@@ -1,11 +1,15 @@
 class polynomial(object):
     def __init__(self,list):
         self.list = list # list is a collection of the coefficients of the polynomial P(x)
-    def order(self):
-        return (len(self.list)-1)
-    def add(self,poly2):
-        k = max(order(self.list),order(poly2))
-        net = []
-        for i in range(k):
-            net.append(self.list[i] + poly2[i])
-        return net
+    def printPoly(self):
+        s = ""
+        for i in range(len(self.list)):
+            if (self.list[i] !=0 and i > 0):
+                s = s + str(self.list[i])+"x"+str(i+1)+ "+ "
+            elif (self.list[i]!= 0 and i == 0):
+                s = s + str(self.list[i])
+        return s
+def main():
+    p = polynomial([2,0,4,-1,6])
+    print(p.printPoly())
+main()
