432cc904ab75ae56e375754e882fe9b8a333a803
sai-advaith <advaith.maddipatla@gmail.com>
2020-01-15 11:49:03 +0000

    add done

--- polynomial.py
+++ polynomial.py
@@ -7,7 +7,23 @@ class polynomial(object):
             if(self.coeff[i] !=0):
                 pos = (i)
         return pos
+    def add(self,polyB):
+        k = min(polyB.order(),self.order())
+        addition = []
+        j = 0
+        while (j<=k):
+            addition.append(polyB.coeff[j]+self.coeff[j])
+            j +=1
+        if (polyB.order()>self.order()):
+            addition.extend(polyB.coeff[j:])
+        elif (polyB.order() < self.order()):
+            addition.extend(self.coeff[j:])
+        return addition
+    
 def main():
-    p = polynomial([0,2,4,5])
-    pB = polynomial([1,2,4,5])
+    p = polynomial([0,2,0,5])
+    pB = polynomial([0,1,2,4,10])
+    print(p.order())
+    print(pB.order())
+    print(p.add(pB))
 main()
