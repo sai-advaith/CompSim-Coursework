68b906b0d3ecfb91524aa4425bb678e147ec3815
sai-advaith <advaith.maddipatla@gmail.com>
2020-01-16 16:47:40 +0000

    main method error

--- Checkpoint 1/test.py
+++ Checkpoint 1/test.py
@@ -1,5 +1,5 @@
 from polynomial import polynomial
-def main():
+if __name__ == "__main__":
     polynomialA = polynomial([2,0,4,-1,0,6]) # representation of 2 + 4x^2 - x^3 + 6x^5
     polynomialB = polynomial([-1,-3,0,4.5]) # representation of -1 -3x + 4.5x^3
     #  if the variable does not appear in the polynomial then its coefficient is 0
@@ -7,4 +7,3 @@ def main():
     print("addition of polynomialA and polynomialB is: ",polynomialA.add(polynomialB).strForm())
     print("first derivative of polynomialA is: ",polynomialA.differentiate().strForm())
     print("Antiderivative of polynomialA is: ", polynomialA.integrate(2).strForm())
-main()
