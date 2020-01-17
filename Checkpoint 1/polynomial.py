d323c04ff6f46fafb248ce176f50437bc558acfc
Advaith Sai <advaith.maddipatla@gmail.com>
2020-01-17 11:22:55 +0000

    documentation

--- Checkpoint 1/polynomial.py
+++ Checkpoint 1/polynomial.py
@@ -3,6 +3,9 @@ class polynomial(object):
         self.coeff = coeff
 # all functions except the first one return an object, which can be easily used to reference to other methods while testing
     def order(self):
+        """
+        The purpose of this function is to determine the order of a polynomial and return an integer
+        """
         order = 0 # the coefficient is taken as zero if the variable does not appear in the equation
         for i in range(len(self.coeff)):
             if(self.coeff[i] !=0):
@@ -10,6 +13,9 @@ class polynomial(object):
         return order # order of the equation
 
     def add(self,polyB):
+        """
+        Adding two polynomials and returning an object which is the result of addition of two polynomials
+        """
         k = min(polyB.order(),self.order()) # the addition will take place only till the length of the smaller list
         addition = polynomial([]) # object will be returned from this method
         j = 0
@@ -23,18 +29,27 @@ class polynomial(object):
         return addition
 
     def differentiate(self):
+        """
+        Calculating the derivative of a polynomial and returning an object containing the derivative of a polynomial
+        """
         diff = polynomial([])
         for i in range(1,len(self.coeff)):
             (diff.coeff).append((self.coeff[i]*(i))) # constant term will become zero in the derivative 
         return diff
 
     def integrate(self,c):
+        """
+        Calculating the anti-derivative of a function and returning an object containing its integral.
+        """
         integrate = polynomial([]) # constant term will be supplied to the method
         for i in range(len(self.coeff)):
             (integrate.coeff).append((self.coeff[i])/(i+1))
         return integrate 
 
     def strForm(self):
+        """
+        Representing a polynomial as a string
+        """
         s = str(self.coeff[0]) # this method attempts to print the objects in string format. 
         for i in range(1,len(self.coeff)):
             if (self.coeff[i] < 0):
