48206fe91640f2355880b19555d69cafdeb68273
sai-advaith <advaith.maddipatla@gmail.com>
2020-01-22 16:40:57 +0000

    radioactive decay

--- Checkpoint 2/radioactive.py
+++ Checkpoint 2/radioactive.py
@@ -1,3 +1,28 @@
 class radiactive(object):
-    def __init__(self,lamb):
-        self.lamb = lamb
+    def  __init__(self,const,matrix,timestep):
+        self.const = const
+        self.matrix = matrix
+        self.timestep = timestep
+    def half_check(self):
+        """
+        boolean function to see if half of the nuclei decayed
+        """
+    def str_matrx(self):
+        """
+        String representation of the 2d array
+        """
+    def decay(self):
+        """
+        In this method we will be showing the decay of the nuclei based on the timestep. return simulated half life
+        """
+    def prob(self):
+        """
+        In this method we will determine the probability of decay
+        """
+    
+if __name__ == "__main__":
+    N = int(input("number of the Iodine-128"))
+    decay_constant = float(input(""))
+
+    
+
