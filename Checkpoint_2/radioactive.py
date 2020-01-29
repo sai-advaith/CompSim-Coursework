ce8f3a51e9c6291f401600e44f80c47563b53b38
sai-advaith <advaith.maddipatla@gmail.com>
2020-01-29 11:46:43 +0000

    error handling

--- Checkpoint_2/radioactive.py
+++ Checkpoint_2/radioactive.py
@@ -1,16 +1,19 @@
 import random
 class Radioactive(object):
+    
     def  __init__(self,const,matrix,timestep):
         self.const = const 
         self.matrix = matrix
         self.timestep = timestep
         self.prob = const*timestep
+    
     def half_check(self):
         """
         Boolean function to see if half of the nuclei decayed
         """
         k  = len(self.matrix) * len(self.matrix)
         return self.decayed() >= k / 2 # function to detect if half_life has been attained
+    
     def __str__(self):
         """
         String representation of the 2d array
@@ -25,14 +28,13 @@ class Radioactive(object):
         print("0: Unedcayed nuclei")
         print("1: Decayed nuclei"+"\n")
         return s
+    
     def decay(self):
         """
         In this method we will be showing the decay of the nuclei based on the timestep. return simulated half life
         """
         half_life = 0
         while(not self.half_check()):
-            # print(half_life)
-            # print()
             for i in range(len(self.matrix)):
                 for j in range(len(self.matrix)):
                     if (self.matrix[i][j] == 0):
@@ -40,6 +42,7 @@ class Radioactive(object):
                             self.matrix[i][j] = 1 # random.random() giving the ranom number between 0 and 1 as probability is never greated than one or less than 0
             half_life += self.timestep # adding timestep after every N nuclei iterated
         return half_life           
+    
     def decayed(self):
         """
         In this method we will be showing the number of decayed atoms
--- Checkpoint_2/test.py
+++ Checkpoint_2/test.py
@@ -5,12 +5,22 @@ if __name__ == "__main__":
     Main method
     """
     N = int(input("number of the Iodine-128: "))
+    if (N<=0):
+        raise ValueError("Negative or zero atoms is not appropriate") #exception handling for negative or zero atoms
+    
     decay_constant = float(input("decay constant: "))
+    if (decay_constant <= 0):
+        raise ValueError("Not possible") #negative decay constants 
+    
     t_step = float(input("timestep: "))
+    if (t_step <= 0):
+        raise ValueError("Inappropriate value of timestep") #negative timestep will result in negative half life which makes no sense 
+
     matrix = [[0 for _  in  range(N)] for _ in range(N)] # matrix with undecayed atoms of size N*N, 0 => Undecayed 1 => Decayed
     iodine = Radioactive(decay_constant,matrix,t_step) #creating iodine object 
     half = np.log(2) / decay_constant # half life of the element based on its decay constant
     k = iodine.decay() # this will decay the atom and store its simulated half life in a variable
+    
     print(str(iodine)) # printing the visualization of the atom
     print("Initial number of undecayed nuclei: ", N*N,"\n") # number of inital undecayed nuclei
     print("Final number of undecayed nuclei: ",N*N - iodine.decayed(),"\n") # final number of undecayed nuclei 
