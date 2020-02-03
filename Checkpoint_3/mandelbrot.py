d521741dc8ecaaae18009bc454258dee75f3afce
sai-advaith <advaith.maddipatla@gmail.com>
2020-02-03 19:13:44 +0000

    mandelbrot

--- Checkpoint_3/mandelbrot.py
+++ Checkpoint_3/mandelbrot.py
@@ -1,3 +1,7 @@
-class Mandelbrot(object):
-    def __init__(self,C):
-        self.C = C
+import numpy as np 
+import matplotlib.pyplot as plt
+class Madelbrot(object):
+    def __init__(self,N,grid):
+        self.N = N
+        self.grid = grid
+    
---
+++ Checkpoint_3/test.py
@@ -0,0 +1,5 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from mandelbrot import Madelbrot
+    if __name__ == "__main__":
+        print("f")
