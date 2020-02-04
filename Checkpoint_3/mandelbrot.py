d71be1dfa26be540e664854f16a144830f083596
sai-advaith <advaith.maddipatla@gmail.com>
2020-02-04 12:09:34 +0000

    core algorithm done

--- Checkpoint_3/mandelbrot.py
+++ Checkpoint_3/mandelbrot.py
@@ -1,7 +1,25 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-class Madelbrot(object):
-    def __init__(self,N,grid):
-        self.N = N
-        self.grid = grid
-    
+class Mandelbrot(object):
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+        self.grid = np.zeros(shape = (width,height))
+    def __str__(self):
+        s = ""
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
+                s = s + str(self.grid[i][j])
+            s = s + "\n"
+        return s
+    def check_mandel(self,c):
+        z = 0
+        N = 0
+        while (N < 255):
+            z = z**2 + c
+            if (abs(z) > 2):
+                break
+            N += 1
+        return N
+        
+
--- Checkpoint_3/test.py
+++ Checkpoint_3/test.py
@@ -1,5 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-from mandelbrot import Madelbrot
-    if __name__ == "__main__":
-        print("f")
+from mandelbrot import Mandelbrot
+if __name__ == "__main__":
+   test = Mandelbrot(255,4,5)
+   print(str(test))
+
