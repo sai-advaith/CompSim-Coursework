91f1efebe8b312f2f6827092887871825616cd75
sai-advaith <advaith.maddipatla@gmail.com>
2020-02-05 15:06:58 +0000

    c4

---
+++ Checkpoint_4/test.py
@@ -0,0 +1,6 @@
+import numpy as np 
+from traffic import Traffic
+if __name__ == "__main__":
+    N = 1
+    road = np.arange(N)
+    ob = Traffic(road)
---
+++ Checkpoint_4/traffic.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+class Traffic(object):
+    def __init__(self,road):
+        self.road = road
+    def movement(self):
+        return self.road
+        
