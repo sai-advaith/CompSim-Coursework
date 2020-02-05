f219fc3b46fabb4633fbc8e686b95cff9a2cbeba
sai-advaith <advaith.maddipatla@gmail.com>
2020-02-05 13:46:03 +0000

    document

--- Checkpoint_3/mandelbrot.py
+++ Checkpoint_3/mandelbrot.py
@@ -3,19 +3,18 @@ import cmath
 import matplotlib.pyplot as plt
 class Mandelbrot(object):
     def __init__(self,width,height,xlow,xhigh,ylow,yhigh):
+        """
+        Constructor
+        """
         self.width = width
         self.height = height
         self.grid = np.zeros(shape = (width,height))
         self.xs = np.linspace(xlow,xhigh,width)
         self.ys = np.linspace(ylow,yhigh,height)
-    # def __str__(self):
-    #     s = ""
-    #     for i in range(len(self.grid)):
-    #         for j in range(len(self.grid[0])):
-    #             s = s + str(self.grid[j][i])
-    #         s = s + "\n"
-    #     return s
     def check_mandel(self,c):
+        """
+        Checking the mandelbrot algorithm
+        """
         z = 0
         N = 0
         while (N < 255):
@@ -25,7 +24,9 @@ class Mandelbrot(object):
             N += 1
         return 0
     def plot_mandel(self):
-
+        """
+        Plotting the values based on the iterative algorithm
+        """
         # keep passing x,y into the check_mandel function. complex(x,y)
         # run a loop across x and y 
         # initialzie it in the grid?
--- Checkpoint_3/test.py
+++ Checkpoint_3/test.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from mandelbrot import Mandelbrot
 if __name__ == "__main__":
+    """
+    Main method
+    """
    test = Mandelbrot(500,500,-2.025,0.6,-1.125,1.125)
    test.plot_mandel()
    
