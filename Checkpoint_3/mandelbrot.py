bf8f884b4b42aa9c43085c8ad7c5bde542bbfad6
sai-advaith <advaith.maddipatla@gmail.com>
2020-02-05 13:45:23 +0000

    document it

--- Checkpoint_3/mandelbrot.py
+++ Checkpoint_3/mandelbrot.py
@@ -5,26 +5,38 @@ class Mandelbrot(object):
     def __init__(self,width,height,xlow,xhigh,ylow,yhigh):
         self.width = width
         self.height = height
-        self.x = (xlow,xhigh)
-        self.y = (ylow,yhigh)
         self.grid = np.zeros(shape = (width,height))
-        self.xs = np.linspace(self.x[0],self.x[1])
-        self.ys = np.linspace(self.y[0],self.y[1])
-    def __str__(self):
-        s = ""
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[0])):
-                s = s + str(self.grid[j][i])
-            s = s + "\n"
-        return s
+        self.xs = np.linspace(xlow,xhigh,width)
+        self.ys = np.linspace(ylow,yhigh,height)
+    # def __str__(self):
+    #     s = ""
+    #     for i in range(len(self.grid)):
+    #         for j in range(len(self.grid[0])):
+    #             s = s + str(self.grid[j][i])
+    #         s = s + "\n"
+    #     return s
     def check_mandel(self,c):
         z = 0
         N = 0
         while (N < 255):
             z = z**2 + c
             if (abs(z) > 2):
-                break
+                return N
             N += 1
-        return N
-        
+        return 0
+    def plot_mandel(self):
+
+        # keep passing x,y into the check_mandel function. complex(x,y)
+        # run a loop across x and y 
+        # initialzie it in the grid?
+        for i in range(self.height):
+            for j in range(self.width):
+                c = complex(self.xs[j],self.ys[i])
+                N = self.check_mandel(c)
+                self.grid[i,j] = N
+        plt.imshow(self.grid, extent=(0,500,0,500))
+        plt.show()
+                
+                
+
 
--- Checkpoint_3/test.py
+++ Checkpoint_3/test.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from mandelbrot import Mandelbrot
 if __name__ == "__main__":
-   test = Mandelbrot(255,4,5)
-   print(str(test))
+   test = Mandelbrot(500,500,-2.025,0.6,-1.125,1.125)
+   test.plot_mandel()
+   
 
