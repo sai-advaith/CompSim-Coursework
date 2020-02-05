6a56d7435489b52fb2923aa8fb22ca197ea7145d
sai-advaith <advaith.maddipatla@gmail.com>
2020-02-05 15:58:07 +0000

    movement done

--- Checkpoint_4/test.py
+++ Checkpoint_4/test.py
@@ -2,5 +2,6 @@ import numpy as np
 from traffic import Traffic
 if __name__ == "__main__":
     N = 1
-    road = np.arange(N)
+    road = np.array([0,1,1,0,1])
     ob = Traffic(road)
+    print(ob.movement())
--- Checkpoint_4/traffic.py
+++ Checkpoint_4/traffic.py
@@ -4,5 +4,23 @@ class Traffic(object):
     def __init__(self,road):
         self.road = road
     def movement(self):
+        k = len(self.road)
+        for i in range(k):
+            if (self.road[i] == 1):
+                if (i < k -1 and self.road[i+1] == 0):
+                    self.road[i+1] = 1
+                    self.road[i] = 0
+                elif ( i == (k - 1) and self.road[0] == 0):
+                    self.road[0] = 1
+                    self.road[i] = 0
+            elif (self.road[i] == 0 and i < k - 1):
+                if (self.road[i] == 0):
+                    if(i == 0 and self.road[k-1] == 1):
+                        self.road[i] = 1
+                        self.road[k-1] = 0
+                    elif(i > 0 and self.road[i-1] == 1):
+                        self.road[i] = 1
+                        self.road[i-1] = 0
+            
         return self.road
-        
+
