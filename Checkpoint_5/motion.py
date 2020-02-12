8e42416c3b9e77a6960ee6ca602b64f8281bc17b
sai-advaith <advaith.maddipatla@gmail.com>
2020-02-12 14:03:42 +0000

    ffs

--- Checkpoint_5/motion.py
+++ Checkpoint_5/motion.py
@@ -10,13 +10,28 @@ class Orbital_Motion(object):
         self.radius_phobos = radius_phobos
         self.G = 6.67*(10)**(-11)
         self.seperation = radius_mars - radius_phobos
-    def mars_phobos(self):
-        distance = abs(self.radius_mars - self.radius_phobos)
-        F = self.G*(self.mass_phobos*self.mass_mars)*distance
+    def mars_phobos_force(self,time):
+        F = self.G*(self.mass_phobos*self.mass_mars)/(self.radius_integration(time))**2
+
         #  to be completed
     def velocity_integration(self,time):
         v_inital = ((self.G * self.mass_mars)/self.seperation)**0.5
         t = 0
+        v = v_inital
+        V = [v_inital]
         for i in range(self.steps):
             t = t + self.length
-            
+            v = v + self.accel_integration(t) * self.length
+            V.append(v)
+    def accel_integration(self,time):
+        return 12111
+
+    def radius_integration(self,time):
+        r_initial = self.seperation
+        t = 0
+        r = r_initial
+        R = [r_initial]
+        for i in range(self.steps):
+            t = t + self.length
+            r = r + self.velocity_integration(t)*self.length
+            R.append(r)
