f412ef44c721ffff8385370bf9765e341169e62e
sai-advaith <advaith.maddipatla@gmail.com>
2020-02-26 14:38:07 +0000

    documentation added for the main method#

--- Checkpoint_4/test.py
+++ Checkpoint_4/test.py
@@ -2,6 +2,9 @@ import numpy as np
 from traffic import Traffic
 from random import randrange
 if __name__ == "__main__":
+    """
+    Main method
+    """
     size = int(input("size of the road: "))
     road = np.zeros(shape=size)
     density = float(input("density of the road: "))
@@ -9,9 +12,9 @@ if __name__ == "__main__":
     cars = size*density
     i = 0
     while(i<cars):
-        pos = randrange(len(road))
-        if (road[pos] == 0):
+        pos = randrange(len(road)) #  getting random position in the road
+        if (road[pos] == 0): #  if the positions is zero then place the car there
             road[pos] = 1
-            i+=1
-    tr = Traffic(cars,road,iterations)
-    tr.update()
+            i+=1 #  increase car numbers placed
+    tr = Traffic(cars,road,iterations) #  creating object
+    tr.update() #  moving the car
